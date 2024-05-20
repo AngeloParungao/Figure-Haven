a01dd9a814da05f27a0e9c5b14916e7ac6c27429
AngeloParungao <angeloparungao.ap@gmail.com>
2024-05-20 15:46:01 +0800

    about us

--- pages/about-us.php
+++ pages/about-us.php
@@ -10,7 +10,33 @@
 </head>
 <body>
     <?php include '../components/navbar.php'?>
-
+    <div class="about-section">
+        <h2>About Us</h2>
+        <span class="header">Welcome to Figure Haven!</span>
+        <p>At Figure Haven, we are passionate about bringing your favorite characters to life. Our mission is to provide high-quality action figures and collectibles that ignite imagination and bring joy to collectors of all ages.</p>
+        <span class="header">Our Story</span>
+        <p>Founded in 2024, Figure Haven started as a small venture by a group of friends who shared a common love for anime and action figures. What began as a hobby has now grown into a thriving business, thanks to our loyal customers and our dedication to quality and customer service.</p>
+        <span class="header">What We Offer</span>
+        <p>We specialize in a wide range of action figures from popular anime series, and movies. Our catalog includes:</p>
+        <div class="catalog-image">
+            <div>
+                <img src="../images/anime-figure.jpg" alt="">
+                <span>Anime Figures</span>
+            </div>
+            <div>
+                <img src="../images/limited.jpg" alt="">
+                <span>Limited Editions</span>
+            </div>
+        </div>
+        <h2>Shop Location</h2>
+        <p>We are located at Baliwag City, Bulacan near at SM City Baliwag</p>
+        <div class="shop-image">
+            <img src="" alt="">
+        </div>
+    </div>
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51114.108345659144!2d120.86868208181019!3d14.971228480168397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339700001fd7ca4b%3A0x48933424030540dd!2sBaliwag%2C%20Bulacan!5e0!3m2!1sen!2sph!4v1716187787318!5m2!1sen!2sph" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 
     <?php include '../components/footer.php'?>
 </body>
